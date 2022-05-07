7c0dd6a93ce0982d712a6abf35c2a099947cb6ea
oktaysavdi <oktay.savdi@gmail.com>
2022-05-07 16:40:25 +0200

    Create variables.tf

---
+++ AKS-Basic/variables.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+  default     = "test"
+}
+
+variable "prefix" {
+  default     = "example"
+}
+
+variable "region" {
+  default     = "northeurope"
+}
+
+variable "size" {
+  type        = string
+  default     = "Standard_D2as_v5"
+}
