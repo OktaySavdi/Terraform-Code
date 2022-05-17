3f529fc90b0dcb399ef6e0cd785e706dbd706f46
oktaysavdi <oktay.savdi@gmail.com>
2022-05-17 17:04:08 +0200

    Create variables.tf

---
+++ K8S-NS-Creation/variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+    default = "small"
+}
+
+variable "team_name" {
+    default = ""
+}
+
+variable "namespace_name" {
+    default = "oktay"
+}
+
+variable "cluster_name" {
+    default = "my-cluster1"
+}
