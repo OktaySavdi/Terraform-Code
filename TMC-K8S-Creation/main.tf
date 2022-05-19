5f2489ed310d29bfae9aaab43e3449d3f7e6254a
oktaysavdi <oktay.savdi@gmail.com>
2022-05-19 14:22:49 +0200

    Create main.tf

---
+++ TMC-K8S-Creation/main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    tanzu-mission-control = {
+      source  = "vmware/tanzu-mission-control"
+      version = "1.0.2"
+    }
+  }
+}
+
+provider "tanzu-mission-control" {
+  endpoint            = var.endpoint == "stg" ? "mytest-cluster.mydomain" : "myprod-cluster.mydomain"
+  vmw_cloud_api_token = var.api_token
+}
+
+// Operations supported : Read, Create, Update & Delete (except nodepools)
+
+// Read Tanzu Mission Control Tanzu Kubernetes Grid Service workload cluster : fetch cluster details
+data "tanzu-mission-control_cluster" "ready_only_cluster_view" {
+  management_cluster_name = var.endpoint == "stg" ? "mytest-cluster_name" : "myprod-cluster_name" #tmc managementcluster list
+  provisioner_name        = var.provisioner_name                                                  # tmc managementcluster provisioner list
+  name                    = var.cluster_name
+}
+
+# Create Tanzu Mission Control Tanzu Kubernetes Grid Service workload cluster entry
+resource "tanzu-mission-control_cluster" "create_tkgs_workload" {
+  management_cluster_name = var.endpoint == "stg" ? "mytest-cluster_name" : "myprod-cluster_name"
+  provisioner_name        = var.provisioner_name
+  name                    = var.cluster_name
+
+  meta {
+    labels = { "environment" : var.endpoint == "stg" ? "test" : "prod" }
+  }
+
+  spec {
+    cluster_group = var.cluster_group
+    tkg_service_vsphere {
+      settings {
+        network {
+          pods {
+            cidr_blocks = [
+              var.network.pods_cidr_blocks,
+            ]
+          }
+          services {
+            cidr_blocks = [
+              var.network.services_cidr_blocks,
+            ]
+          }
+        }
+      }
+
+      distribution {
+        version = var.k8s_version
+      }
+
+      topology {
+        control_plane {
+          class             = var.class.control_plane_class
+          storage_class     = var.class.control_plane_storage_class
+          high_availability = var.node_count.control_plane_high_availability
+          volumes {
+            capacity          = 4
+            mount_path        = "/var/lib/etcd"
+            name              = "etcd-0"
+            pvc_storage_class = "tkgs-k8s-obj-policy"
+          }
+        }
+        node_pools {
+          spec {
+            worker_node_count = var.node_count.worker_node_count
+            node_label = {
+              "environment" : var.endpoint == "stg" ? "test" : "prod"
+            }
+
+            tkg_service_vsphere {
+              class         = var.class.worker_node_class
+              storage_class = var.class.worker_node_storage_class
+              volumes {
+                capacity          = 4
+                mount_path        = "/var/lib/etcd"
+                name              = "etcd-0"
+                pvc_storage_class = "tkgs-k8s-obj-policy"
+              }
+            }
+          }
+          info {
+            name        = var.nodepool_info.name
+            description = var.nodepool_info.description
+          }
+        }
+      }
+    }
+  }
+}
